302b647edfc1b019dbeff4329a06d10f51c91ed8
taisiie <anastasia.geleta@estudiantat.upc.edu>
2025-11-23 21:44:43 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+from flask import render_template  
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index4.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002)
